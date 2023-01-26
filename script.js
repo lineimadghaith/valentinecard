2d129a3a4d323017e32f1da9cb4358ab3243d749
lineimadghaith <123649910+lineimadghaith@users.noreply.github.com>
2023-01-26 18:14:09 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+function function1() {
+ document.getElementById("firstpage").style.display="none";
+ document.getElementById("secondpage").style.display="block";
+}
+function function2() {
+ document.getElementById("firstpage").style.display="block";
+ document.getElementById("secondpage").style.display="none";
+}
